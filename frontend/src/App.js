2bd11c208b190537ca7a3fe78229288b41d628ca
Aman <amanloc1@hotmail.com>
2020-08-05 19:11:52 +0530

    added sample compnent and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Route, Switch, BrowserRouter } from 'react-router-dom';  //uncomment this line
+import SampleComponent from './components/Sample.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <BrowserRouter > //uncomment this line
+
+
+    // <switch>  //uncomment this line
+
+    //you can test your component here by similar structure
+    <SampleComponent />  //comment this line
+
+    // <Route path="/" component={SampleComponent} /> //uncomment this line
+    // </switch> //uncomment this line 
+
+    // </BrowserRouter > //uncomment this line
   );
 }
 
---
+++ frontend/src/components/Sample.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SampleComponent() {
+    return (
+        <div>
+            This is a sample component.
+        </div>
+    )
+}
+
+export default SampleComponent;
